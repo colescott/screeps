ff2f558fb6fdb073c73cd64f958ee0775404d27a
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-04 21:16:34 -0800

    fix example config

--- config.example.js
+++ config.example.js
@@ -1,12 +1,13 @@
 module.exports = {
     spawn: {
-        builder: 2,
-        upgrader: 2,
-        harvester: 2
+        targetBuilders: 3,
+        targetUpgraders: 5,
+        targetHarvesters: 2
     },
     spawn_types: {
         builder: [WORK, CARRY, CARRY, MOVE, MOVE],
         upgrader: [WORK, CARRY, CARRY, MOVE, MOVE],
         harvester: [WORK, CARRY, CARRY, MOVE, MOVE]
-    }
+    },
+    spawner_name: "Spawn1"
 };
