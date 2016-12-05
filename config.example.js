0df0faf13b2788af124775d549e399e8208f7945
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-04 20:53:39 -0800

    config-more

--- config.example.js
+++ config.example.js
@@ -3,5 +3,10 @@ module.exports = {
         builder: 2,
         upgrader: 2,
         harvester: 2
+    },
+    spawn_types: {
+        builder: [WORK, CARRY, CARRY, MOVE, MOVE],
+        upgrader: [WORK, CARRY, CARRY, MOVE, MOVE],
+        harvester: [WORK, CARRY, CARRY, MOVE, MOVE]
     }
 };
