6a2ea290be0bc23ba283a95a19a9aec4f4b03264
Cole Scott <colescottsf@gmail.com>
2016-12-05 09:55:31 -0800

    Fix harvesters to completely unload energy

--- role.harvester.js
+++ role.harvester.js
@@ -2,7 +2,13 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
+        if(!creep.memory.harvesting && creep.carry.energy == 0) {
+            creep.memory.harvesting = true;
+        }
+        if(creep.memory.harvesting && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.harvesting = false;
+        }
+        if(creep.memory.harvesting) {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
