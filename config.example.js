87a9fe7ba67f93d0dc3a135559354217c1fbebb2
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-04 20:38:23 -0800

    add config

---
+++ config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+    spawn: {
+        builder: 2,
+        upgrader: 2,
+        harvester: 2
+    }
+};
--- main.js
+++ main.js
@@ -3,10 +3,6 @@ var spawn_creeps = require('task.spawncreeps');
 var assign_roles = require('task.assign_roles');
 
 module.exports.loop = function () {
-    totalHarvesters = 0;
-    totalUpgraders = 0;
-    totalBuilders = 0;
-
     clear_memory();
     spawn_creeps();
     assign_roles();
