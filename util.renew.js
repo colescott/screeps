17ff730ad168a8fd850879a5b6619ce715481a81
heathexer <heathexer@gmail.com>
2016-12-05 20:52:53 -0800

    quick fix

--- util.renew.js
+++ util.renew.js
@@ -1,23 +1,23 @@
 const {
-  regenAt,
-  spawner_name
+    regenAt,
+    spawner_name
 } = require('config');
 
 module.exports = () => {
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if(creep.ticksToLive < regenAt && (Game.spawns[spawner_name].renewCreep(creep) != ERR_NOT_ENOUGH_ENERGY)) {
-      creep.memory.renewing = true;
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.ticksToLive < regenAt && (Game.spawns[spawner_name].renewCreep(creep) != ERR_NOT_ENOUGH_ENERGY)) {
+        creep.memory.renewing = true;
+        }
+        if((creep.ticksToLive > 1000) || Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_ENOUGH_ENERGY) {
+            creep.memory.renewing = false;
+        }
+        if(creep.memory.renewing) {
+            if(Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns[spawner_name]);
+            } else {
+                Game.spawns[spawner_name].renewCreep(creep);
+            }
+        }
     }
-    if((creep.ticksToLive > 1000) || Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_ENOUGH_ENERGY) {
-      creep.memory.renewing = false;
-    }
-    if(creep.memory.renewing) {
-      if(Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(Game.spawns[spawner_name]);
-      } else {
-        Game.spawns[spawner_name].renewCreep(creep);
-      }
-    }
-  }
 }
