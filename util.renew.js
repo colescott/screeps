bfa4f25edf8d52909af699bae25127edc191ecf3
heathexer <heathexer@gmail.com>
2016-12-05 20:49:22 -0800

    quick fix

--- util.renew.js
+++ util.renew.js
@@ -9,7 +9,7 @@ module.exports = () => {
     if(creep.ticksToLive < regenAt && (Game.spawns[spawner_name].renewCreep(creep) != ERR_NOT_ENOUGH_ENERGY)) {
       creep.memory.renewing = true;
     }
-    if((creep.memory.renewing && creep.ticksToLive > 1000) || Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_ENOUGH_ENERGY) {
+    if((creep.ticksToLive > 1000) || Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_ENOUGH_ENERGY) {
       creep.memory.renewing = false;
     }
     if(creep.memory.renewing) {
