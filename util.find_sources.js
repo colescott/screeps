da6282feaea7c0898c902745ebdd80861d5694b9
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-05 20:18:35 -0800

    add option to remove the added option

--- util.find_sources.js
+++ util.find_sources.js
@@ -1,4 +1,12 @@
+const {
+    source_rotate,
+    source_id
+} = require("config");
+
 module.exports = (creep) => {
     const sources = creep.room.find(FIND_SOURCES)
-    return sources[Math.floor(Math.random()*sources.length)].id;
+    if (source_rotate) {
+        return sources[Math.floor(Math.random()*sources.length)].id;
+    }
+    return sources[source_id].id;
 }
