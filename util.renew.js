97f802752cca3e96b07f2689c149992e553241e6
Cole Scott <colescottsf@gmail.com>
2016-12-06 17:42:44 -0800

    Fix multiple regen calls and regenning without need

--- util.renew.js
+++ util.renew.js
@@ -7,16 +7,18 @@ module.exports = () => {
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
         if(creep.ticksToLive < regenAt && (Game.spawns[spawner_name].renewCreep(creep) != ERR_NOT_ENOUGH_ENERGY)) {
-        creep.memory.renewing = true;
+            creep.memory.renewing = true;
         }
-        if((creep.ticksToLive > 1000) || Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_ENOUGH_ENERGY) {
+        if((creep.ticksToLive > 1000)) {
             creep.memory.renewing = false;
         }
         if(creep.memory.renewing) {
-            if(Game.spawns[spawner_name].renewCreep(creep) == ERR_NOT_IN_RANGE) {
+            var error = Game.spawns[spawner_name].renewCreep(creep)
+
+            if(error == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.spawns[spawner_name]);
-            } else {
-                Game.spawns[spawner_name].renewCreep(creep);
+            } else if(error == ERR_NOT_ENOUGH_ENERGY) {
+                creep.memory.renewing = false;
             }
         }
     }
