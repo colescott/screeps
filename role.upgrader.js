420b478ae13e91a5c2858a319e528dfa78bfc9d3
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-07 18:21:11 -0800

    upgrader upgraded

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,6 +2,7 @@ const config = require('config');
 const renew = require('util.renew');
 const get_energy = require('util.get_energy');
 const find_sources = require('util.find_sources');
+const RoleBase = require('role.base');
 
 const {
     source_id,
@@ -9,58 +10,23 @@ const {
     regenAt
 } = config;
 
-var roleUpgrader = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        //Setup
-        if (!creep.memory.setup) {
-            creep.memory.setup = true;
-            roleUpgrader.setup(creep);
-        }
-        // Assuming it isn't fixing itself
-        if(!creep.memory.renewing) {
-            if(creep.memory.upgrading && creep.carry.energy == 0) {
-                roleUpgrader.energyMode(creep);
-                creep.memory.upgrading = false;
-            }
-            if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-                roleUpgrader.taskMode(creep);
-                creep.memory.upgrading = true;
-            }
-            if(creep.memory.upgrading) {
-                roleUpgrader.doTask(creep);
-            }
-            else {
-                roleUpgrader.getEnergy(creep);
-            }
-        }
-    },
-    setup: function(creep) {
-        creep.memory.source = find_sources(creep);
-    },
-    doTask: function(creep) {
-        const res = creep.upgradeController(creep.room.controller);
+class RoleUpgrader extends RoleBase {
+    setup() {
+        this.creep.memory.source = find_sources(creep);
+    }
+    getEnergy() {
+        get_energy(this.creep);
+    }
+    work() {
+        const res = this.creep.upgradeController(this.creep.room.controller);
         if(res == ERR_NOT_IN_RANGE) {
-            creep.moveTo(creep.room.controller);
+            this.creep.moveTo(this.creep.room.controller);
         }
-    },
-    getEnergy: function(creep) {
-        get_energy(creep);
-    },
-    energyMode: function(creep) {
-        creep.memory.source = find_sources(creep);
-    },
-    taskMode: function(creep) {}
-};
-
-function RoleUpgrader(creep) {
-    this.creep = creep;
-    this.memory = creep.memory;
-}
-
-RoleUpgrader.prototype.run = function() {
-    roleUpgrader.run(this.creep);
+    }
+    switchToWork() {}
+    switchToEnergy() {
+        this.memory.source = find_sources(creep);
+    }
 }
 
 module.exports = RoleUpgrader;
