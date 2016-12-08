0000c11935181030267fd3f212e8fbf3f3590327
Elias Lawson-Fox <eliaslfox@gmail.com>
2016-12-07 18:08:11 -0800

    refactored harvester

--- role.builder.js
+++ role.builder.js
@@ -9,56 +9,6 @@ const {
     wallTarget
 } = config;
 
-var roleBuilder = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if(!creep.memory.renewing) {
-          if(creep.memory.building && creep.carry.energy == 0) {
-              creep.memory.building = false;
-          }
-          if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-              creep.memory.building = true;
-          }
-
-          if(creep.memory.building) {
-              var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-              if(targets.length) {
-                  if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                      creep.moveTo(targets[0]);
-                  }
-              } else {
-                  // Repair stuff if no construction sites found
-                  var structures = creep.room.find(FIND_STRUCTURES);
-                  for(var index in structures)
-                  {
-                      var structure = structures[index];
-
-                      if(structure.structureType == STRUCTURE_WALL || structure.structureType == STRUCTURE_RAMPART)
-                      {
-                          if(structure.hits < wallTarget) {
-                              if(creep.repair(structure) == ERR_NOT_IN_RANGE) {
-                                  creep.moveTo(structure);
-                              }
-                              break;
-                          }
-                      } else
-                      if(structure.hits < structure.hitsMax) {
-                          if(creep.repair(structure) == ERR_NOT_IN_RANGE) {
-                              creep.moveTo(structure);
-                          }
-                          break;
-                      }
-                  }
-              }
-          }
-          else {
-                get_energy(creep);
-          }
-      }
-    }
-};
-
 class RoleBuilder extends RoleBase {
     constructor(creep) {
         super(creep);
