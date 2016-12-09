f2698c420cef4e067231a660149998ba3cb62cab
heathexer <heathexer@gmail.com>
2016-12-08 19:47:38 -0800

    Add actual file

---
+++ util.count_creeps.js
@@ -0,0 +1,10 @@
+module.exports = (type) => {
+    var res = 0;
+    for(name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep && (creep.memory.role == type || type == null)) {
+            res++;
+        }
+    }
+    return res;
+}
