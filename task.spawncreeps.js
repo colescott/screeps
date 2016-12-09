fa0e28492df88fb1cee276b9cdcd498873da6f69
heathexer <heathexer@gmail.com>
2016-12-08 21:35:54 -0800

    Fix someone else's fucked formatting

--- task.spawncreeps.js
+++ task.spawncreeps.js
@@ -11,14 +11,11 @@ module.exports = () => {
     const spawner_name = config.spawner_name;
     if(countCreeps() == 0 && (Game.spawns[spawner_name].canCreateCreep([WORK, CARRY, CARRY, MOVE, MOVE]) == OK)) {
         Game.spawns[ spawner_name ].createCreep([WORK, CARRY, CARRY, MOVE, MOVE], `EHarvester`, { role: "harvester" });
-    } else
-    if(countCreeps('harvester') < targetHarvesters && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.harvester) == OK)) {
+    } else if(countCreeps('harvester') < targetHarvesters && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.harvester) == OK)) {
         Game.spawns[ spawner_name ].createCreep(config.spawn_types.harvester, `Harvester${  Math.floor(Math.random() * 1024)}`, { role: "harvester" });
-    } else
-    if(countCreeps('upgrader') < targetUpgraders && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.upgrader) == OK)) {
+    } else if(countCreeps('upgrader') < targetUpgraders && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.upgrader) == OK)) {
         Game.spawns[ spawner_name ].createCreep(config.spawn_types.upgrader, `Upgrader${  Math.floor(Math.random() * 1024)}`, { role: "upgrader" });
-    } else
-    if(countCreeps('builder') < targetBuilders && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.builder) == OK)) {
+    } else if(countCreeps('builder') < targetBuilders && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.builder) == OK)) {
         Game.spawns[ spawner_name ].createCreep(config.spawn_types.builder, `Builder${  Math.floor(Math.random() * 1024)}`, { role: "builder" });
     }
 };
