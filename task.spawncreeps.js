bc046d21980ac32cbce339e639025c1388313b65
heathexer <heathexer@gmail.com>
2016-12-08 21:20:32 -0800

    Add emergency cheap harvester to spawn if all creeps are dead

--- task.spawncreeps.js
+++ task.spawncreeps.js
@@ -9,6 +9,9 @@ const {
 
 module.exports = () => {
     const spawner_name = config.spawner_name;
+    if(countCreeps() == 0 && (Game.spawns[spawner_name].canCreateCreep([WORK, CARRY, CARRY, MOVE, MOVE]) == OK)) {
+        Game.spawns[ spawner_name ].createCreep([WORK, CARRY, CARRY, MOVE, MOVE], `EHarvester`, { role: "harvester" });
+    } else
     if(countCreeps('harvester') < targetHarvesters && (Game.spawns[spawner_name].canCreateCreep(config.spawn_types.harvester) == OK)) {
         Game.spawns[ spawner_name ].createCreep(config.spawn_types.harvester, `Harvester${  Math.floor(Math.random() * 1024)}`, { role: "harvester" });
     } else
